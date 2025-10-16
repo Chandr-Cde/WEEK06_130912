262cf86f89c5456e4cf6fe67f84b3e444f22ad77
user <user@CpfnajlalnfpakfF>
2025-10-16 22:08:56 +0700

    handphone

--- week06_130912/src/week06/chandra/id/ac/umn/Handphone.java
+++ week06_130912/src/week06/chandra/id/ac/umn/Handphone.java
@@ -1,8 +1,23 @@
 package week06.chandra.id.ac.umn;
 
-public class Handphone {
+public class Handphone extends Barang{
+	private String warna;
+	public static int total;
+	
+	public Handphone(int id, String nama, double harga, int stok, String warna) {
+		super(id, nama, harga, stok);
+		this.warna = warna;
+	}
+	
+		public String getWarna() {
+		return warna;
+	}
 
-	public static void main(String[] args) {
+	public void setWarna(String warna) {
+		this.warna = warna;
+	}
+
+		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
