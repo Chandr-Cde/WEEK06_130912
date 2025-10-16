a3c63f6458532f4305b4a0e60481692a825f897a
user <user@CpfnajlalnfpakfF>
2025-10-16 22:09:12 +0700

    voucher

---
+++ week06_130912/src/week06/chandra/id/ac/umn/Voucher.java
@@ -0,0 +1,20 @@
+package week06.chandra.id.ac.umn;
+
+public class Voucher extends Barang {
+	private double pajak;
+	public static int total;
+	public double hargaJual;
+	
+	public Voucher(int id, String nama, double harga, int stok, double pajak, double hargaJual) {
+		super(id, nama, harga, stok);
+		this.pajak = pajak;
+		this.hargaJual = hargaJual;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
