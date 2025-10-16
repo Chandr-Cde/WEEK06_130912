1d88de42fe15a830731ad72c0de5c0e14c7ff52b
user <user@CpfnajlalnfpakfF>
2025-10-16 22:07:40 +0700

    Barang

--- week06_130912/src/week06/chandra/id/ac/umn/Barang.java
+++ week06_130912/src/week06/chandra/id/ac/umn/Barang.java
@@ -2,33 +2,49 @@ package week06.chandra.id.ac.umn;
 
 public class Barang {
 	protected int id;
-	protected int stok;
 	protected double harga;
-	protected String name;
+	protected String nama;
+	protected int stok;
 	
-	
-	public Barang(String name, double harga, int id, int stok) {
-		this.name = name;
-		this.harga = harga;
-		this.id = id;
-		this.stok = stok;
+	public Barang(int id, String nama, double harga, int stok) {
+	    this.id = id;
+	    this.nama = nama;
+	    this.harga = harga;
+	    this.stok = stok;
 	}
 	
-	public String getName() {
-		return name;
-	}
-	public double getHarga() {
-		return harga;
-	}
 	public int getId() {
 		return id;
 	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getHarga() {
+		return harga;
+	}
+
+	public void setHarga(double harga) {
+		this.harga = harga;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
 	public int getStok() {
 		return stok;
 	}
-	
-	
-	
+
+	public void setStok(int stok) {
+		this.stok = stok;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
